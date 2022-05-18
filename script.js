0e15d22d1e1e8f4de6f8e53d2991583843ed9f37
sancho-sumy <59047465+sancho-sumy@users.noreply.github.com>
2022-05-19 00:21:27 +0300

    Modify code

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
+let completePhrase = '';
+let decoration = '';
+
 const messageObj = {
     messageFirstPart: ['Soon', 'In future', 'Next year', 'Next month', 'Some day', 'In other life'],
     messageSecondPart: ['you', 'your friend', 'someone'],
     messageThirdPart: ['meet new friend', 'have a trip', 'make dream true', 'get a new job', 'change lifestyle', 'buy a boat', 'go to the Mars', 'become poppular', 'buy a sport car'],
+    phraseConstructor() {                                                       //Complete phrase generator.
+        let firstPart = randomSelection(messageObj.messageFirstPart);
+        let secondPart = randomSelection(messageObj.messageSecondPart);
+        let thirdPart = randomSelection(messageObj.messageThirdPart);
+        completePhrase = `${firstPart}, ${secondPart} will ${thirdPart}!`;
+    },
 }
-
-const decorationList = ['=', '-', '~'];
-
-let completePhrase = '';
-let decoration = '';
 
 //Function for random selection of some text from specified array.
 const randomSelection = (inputArray) => {
     return inputArray[Math.floor(Math.random() * inputArray.length)];
 }
 
-//Complete phrase generator.
-
-const phraseConstructor = () => {
-    let firstPart = randomSelection(messageObj.messageFirstPart);
-    let secondPart = randomSelection(messageObj.messageSecondPart);
-    let thirdPart = randomSelection(messageObj.messageThirdPart);
-    completePhrase = `${firstPart}, ${secondPart} will ${thirdPart}!`;
-}
-
 //Add some decoration to our output. Decoration length will be changed to fit generated phrase. Decoration symbol will be selected randomly from provided array.
 
 const decorationLine = () => {
+    const decorationList = ['=', '-', '~'];
     let decorationArray = [];
     let decorationSymbol = randomSelection(decorationList);
     let decorationLength = completePhrase.length + 4;
@@ -35,7 +31,7 @@ const decorationLine = () => {
     decoration = decorationArray.join('');
 }
 
-phraseConstructor();
+messageObj.phraseConstructor()
 decorationLine()
 
 console.log(`\nYour prediction:\n${decoration}\n| ${completePhrase} |\n${decoration}`);
