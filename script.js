624c73bf2de521a8faebb719a5df6139f5af1067
sancho-sumy <59047465+sancho-sumy@users.noreply.github.com>
2022-05-18 23:22:46 +0300

    Modify script.js

--- script.js
+++ script.js
@@ -4,33 +4,36 @@ const messageObj = {
     messageThirdPart: ['meet new friend', 'have a trip', 'make dream true', 'get a new job', 'change lifestyle', 'buy a boat', 'go to the Mars', 'become poppular', 'buy a sport car'],
 }
 
+const decorationList = ['=', '-', '~', '+', ':'];
+
 let completePhrase = '';
 
 //Function for random selection of some text from specified array.
-const randomTextSelection = (inputArray) => {
+const randomSelection = (inputArray) => {
     return inputArray[Math.floor(Math.random() * (inputArray.length - 1))];
 }
 
 //Complete phrase generator.
 
 const phraseConstructor = () => {
-    let firstPart = randomTextSelection(messageObj.messageFirstPart);
-    let secondPart = randomTextSelection(messageObj.messageSecondPart);
-    let thirdPart = randomTextSelection(messageObj.messageThirdPart);
+    let firstPart = randomSelection(messageObj.messageFirstPart);
+    let secondPart = randomSelection(messageObj.messageSecondPart);
+    let thirdPart = randomSelection(messageObj.messageThirdPart);
     completePhrase = `${firstPart}, ${secondPart} will ${thirdPart}!`;
 }
 
-//Add some decoration to our output. Decoration length will be changed to fit generated phrase. 
+//Add some decoration to our output. Decoration length will be changed to fit generated phrase. Decoration symbol will be selected randomly from provided array.
 
-const decorationLine = (char) => {
+const decorationLine = () => {
     let decorationArray = [];
+    let decorationSymbol = randomSelection(decorationList);
     let decorationLength = completePhrase.length + 4;
     for (let i = 0; i < completePhrase.length + 4; i++) {
-        decorationArray.push(char);
+        decorationArray.push(decorationSymbol);
     }
     return decorationArray.join('');
 }
 
 phraseConstructor();
 
-console.log(`\nYour prediction:\n${decorationLine('~')}\n| ${completePhrase} |\n${decorationLine('=')}`);
+console.log(`\nYour prediction:\n${decorationLine()}\n| ${completePhrase} |\n${decorationLine()}`);
